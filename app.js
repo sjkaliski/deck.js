7d2e89a3b75c849657751e0eb9777b99fc30dc96
David Byrd <david@byrdhou.se>
2012-09-23 03:11:39 -0400

    we got creating a table done

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
-  mongoose.connect('mongodb://david:bellarmine@alex.mongohq.com:10070/ferrisclothiers');
+  mongoose.connect('mongodb://localhost:27017/deckdev');
+  //mongoose.connect('mongodb://eboard:shipping@alex.mongohq.com:10070/ferrisclothiers');
 });
 
 app.configure('development', function(){
@@ -37,11 +38,12 @@ var Card = mongoose.model('Card', new mongoose.Schema({
 }));
 
 var User = mongoose.model('User', new mongoose.Schema({
-  name: {type: String, trim: true, required: true },
+  name: String,
   cards: [Card]
 }));
 
 var Table = mongoose.model('Table', new mongoose.Schema({
+  name: String,
   users: [User],
   cards: [Card]
 }));
@@ -49,27 +51,38 @@ var Table = mongoose.model('Table', new mongoose.Schema({
 
 
 app.get('/', function(req, res) {
-  //This is a form to create a new table
-
-
   res.render('index', { title: 'Deck.js' });
 });
 
 app.post('/table/create', function(req, res){
-  //create the first user
-  res.redirect("/");
+  //creates a new table and redirect you to that table
+  var table = new Table();
+  table.name = req.body.tablename;
+  table.save(function(err, doc){
+    if(err) throw err;
+    res.redirect('/table/'+doc._id);
+  });
 });
-app.get('/table/:id', function(req, res) {
 
-});
-app.get('/table/:id/:user', function(req, res) {
-  res.send("todo");
+app.get('/table/:id', function(req, res) {
+  Table.findById(req.params.id, function(err, doc){
+    res.send(doc.name);
+  });
 });
 
 app.get('/table/:id/user/create', function(req, res){
   //This is the url that the table creator sends to his friends
   //They will then sign up with username, etc
-  res.send("todo");
+  
+  Table.findById(req.params.id, function(err, table){
+    var user = new User();
+    user.name = req.body.username;
+    user.save(function(err, doc){
+      table.users.push(doc);
+      res.send('sucess');
+    });
+    
+  });
 });
 
 app.post('/table/:id/user/create', function(req, res){
